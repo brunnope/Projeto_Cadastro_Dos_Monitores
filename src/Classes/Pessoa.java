45e74832daff4bf5d93c4b29420cdf310cc4a96e
Anderson da Silva Santos <anderson.ss1090@gmail.com>
2023-11-15 17:40:36 -0300

    commit superclasse pessoa
    adicionada a superclasse pessoa e classe coordenador, atualização da classe aluno

---
+++ src/Classes/Pessoa.java
@@ -0,0 +1,57 @@
+package Classes;
+
+public class Pessoa {
+	private String nome;
+	private String email;
+	private String senha;
+	private Sexo sexo;
+	
+	public Pessoa(String nome, String email, String senha, Sexo sexo) {
+		this.nome = nome;
+		this.email = email;
+		this.senha = senha;
+	}
+	public Pessoa(String nome, String email, String senha) {
+		this.nome = nome;
+		this.email = email;
+		this.senha = senha;
+	}
+	public Pessoa(String email, String senha) {
+		this.email = email;
+		this.senha = senha;
+	}
+	public Pessoa() {
+		
+	}
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Sexo getSexo() {
+		return sexo;
+	}
+
+	public void setSexo(Sexo sexo) {
+		this.sexo = sexo;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}	
+}
