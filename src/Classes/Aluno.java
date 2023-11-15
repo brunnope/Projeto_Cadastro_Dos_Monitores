f4dda6338a9da53a194e2857586b349cde0f8ac4
Anderson da Silva Santos <anderson.ss1090@gmail.com>
2023-11-15 17:40:36 -0300

    commit superclasse pessoa
    adicionada a superclasse pessoa e classe coordenador, atualização da classe aluno

--- src/Classes/Aluno.java
+++ src/Classes/Aluno.java
@@ -1,27 +1,19 @@
 package Classes;
-public class Aluno {
-	private String nome;
-	private Sexo sexo;
+public class Aluno extends Pessoa{
 	private String matricula;
-	private String email;
-	private String senha;
 	
 	public Aluno(String nome, Sexo sexo, String matricula, String email, String senha) {
-		this.nome = nome;
-		this.sexo = sexo;
+		super(nome, email, senha, sexo);
 		this.matricula = matricula;
-		this.email = email;
-		this.senha = senha;
+		
 	}
 	
 	public Aluno(String nome, String matricula, String email, String senha) {
-		this.nome = nome;
+		super(nome, email, senha);
 		this.matricula = matricula;
-		this.email = email;
-		this.senha = senha;
 	}
 	
-	public Aluno() {
+	public Aluno(){
 		
 	}
 	
@@ -32,39 +24,7 @@ public class Aluno {
 			"Email: " + getEmail() + "\n";
 		}
 
-	public String getNome() {
-		return nome;
-	}
-
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-
-	public Sexo getSexo() {
-		return sexo;
-	}
-
-	public void setSexo(Sexo sexo) {
-		this.sexo = sexo;
-	}
-
 	public String getMatricula() {
 		return matricula;
 	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public String getSenha() {
-		return senha;
-	}
-
-	public void setSenha(String senha) {
-		this.senha = senha;
-	}	
 }
---
+++ src/Classes/Coordenador.java
@@ -0,0 +1,12 @@
+package Classes;
+
+public class Coordenador extends Pessoa {
+	
+	public Coordenador(String email, String senha) {
+		super(email, senha);
+		
+	}
+	public Coordenador(String nome, String email, String senha) {
+		super(nome, email, senha);
+	}
+}
