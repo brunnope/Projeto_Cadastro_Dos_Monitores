f53d60fa916679684a28d2d88163c1e79c53bd40
Anderson da Silva Santos <anderson.ss1090@gmail.com>
2023-11-28 00:01:02 -0300

    Ouvinte telaLogin

--- src/Telas/TelaLogin.java
+++ src/Telas/TelaLogin.java
@@ -1,14 +1,23 @@
 package Telas;
 
 import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
+
+import Classes.Aluno;
+import Classes.Coordenador;
+import Classes.Pessoa;
+import Excecoes.CredenciaisInvalidasException;
+import Excecoes.NenhumAlunoCadastradoException;
 import Telas.FabricaComponentes.*;
 
 public class TelaLogin extends TelaPadrao{
-
+	public JTextField tLogin;
+	public JTextField tSenha;
 	public TelaLogin() {
 		super("LOGIN");
 		
@@ -41,13 +50,10 @@ public class TelaLogin extends TelaPadrao{
 	}
 
 	private void adicionarTextFields() {
-		JTextField tLogin = FabricaJTextField.criarJTextField(370, 375, 200, 30, Color.WHITE, Color.BLACK, 12, Color.GRAY);
+		tLogin = FabricaJTextField.criarJTextField(370, 375, 200, 30, Color.WHITE, Color.BLACK, 12, Color.GRAY);
 		tLogin.setToolTipText("Exemplo: brunno@academico.ifpb.edu.br");
 		add(tLogin);
-		
-		
-		JTextField tSenha = FabricaJTextField.criarJPasswordField(370, 430, 200, 30, Color.WHITE, Color.BLACK, 12, Color.GRAY);
-		
+		tSenha = FabricaJTextField.criarJPasswordField(370, 430, 200, 30, Color.WHITE, Color.BLACK, 12, Color.GRAY);
 		tSenha.setToolTipText("Exemplo: brunno123");
 		add(tSenha);
 		
@@ -58,6 +64,30 @@ public class TelaLogin extends TelaPadrao{
 	private void adicionarButtons() {
 		JButton bLogin = FabricaJButton.criarJButton("Login", 336, 470, 234, 30, Color.GREEN, Color.WHITE, 12);
 		add(bLogin);
+		bLogin.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				String login = tLogin.getText();
+				String senha = tSenha.getText();
+				try {
+					Pessoa pessoaLogada = menu.login(login, senha);
+					if (pessoaLogada != null) {
+						System.out.println("Login bem-sucedido para " + pessoaLogada.getNome() + "!\n");
+						if (pessoaLogada instanceof Coordenador) {
+							//codigo coordenador
+						}else if (pessoaLogada instanceof Aluno);
+						//codigo aluno	
+					}
+				} catch (NenhumAlunoCadastradoException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				} catch (CredenciaisInvalidasException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+			}
+		});
 		
 		JButton bEsqueciSenha = FabricaJButton.criarJButton("Esqueci a Senha", 336, 530, 115, 30, Color.GREEN, Color.WHITE, 12);
 		add(bEsqueciSenha);
